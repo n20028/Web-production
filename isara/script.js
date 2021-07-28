d3175a0653548b0ac06d2b5adabf6210de24975d
n20028 <n20028@std.it-college.ac.jp>
2021-07-28 23:25:08 +0900

    class flowまで終了

--- isara/script.js
+++ isara/script.js
@@ -5,4 +5,11 @@ $(function () {
 	setTimeout(function(){
 		$('.start').fadeOut(500);
 	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+
+	$('.flow-show-box-left').click(function() {
+		$(this).siblings('.flow-show-box-right').slideToggle()
+		$(this).find('.open-tag').toggleClass('trans');
+		$('.flow-show-box-left').not($(this)).siblings('.flow-show-box-right').slideUp();
+		$('.flow-show-box-left').not($(this)).find('.open-tag').removeClass('trans');
+	});
 })
